c1cb55b65a17f3662b2eac491d98b01acb27f338
toru.yamada <1194595t@gmail.com>
2019-11-04 02:26:41 +0000

    add pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(10..1000).each do |number|
+  Task.create(status: 'doing ' + number.to_s, content: 'test content ' + number.to_s)
+end
+
+#Task.create(content: 'test content1', status: 'testing1')
