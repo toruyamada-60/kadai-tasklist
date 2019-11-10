013c08f8fd67e18ba979d3e25fe86568793cd415
toru.yamada <1194595t@gmail.com>
2019-11-10 04:42:53 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,20 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:destroy]
   
   def index
-    @tasks = Task.order(id: :desc).page(params[:page])
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
       if @task.save
         flash[:success] = 'タスクが作成されました'
-        redirect_to @task
+        redirect_to root_url
       else
         flash.now[:danger] = 'タスクが作成されませんでした'
         render :new
@@ -37,12 +41,10 @@ class TasksController < ApplicationController
       end
   end
   
-
   def destroy
     @task.destroy
-    
     flash[:success] = 'タスクが削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -56,4 +58,10 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   
+  root to: 'tasks#index'
+  
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
