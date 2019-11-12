3f58c2f5397984035ea97ae1dd62e00b31a3d082
toru.yamada <1194595t@gmail.com>
2019-11-12 09:48:50 +0000

    for heroku3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:show, :destroy]
+  before_action :require_user_logged_in, only: [:index, :show]
   
   def index
     if logged_in?
@@ -50,7 +51,7 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
   end
   
   # strong parameter
